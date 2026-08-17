c42f8b55437d9a58c8a3ddc64981399a4555de0b
Abdullah Al Faruk <alfaruk091@gmail.com>
2026-08-17 10:17:14 +0600

    added final encapsulation file

--- Encapsulation/ConditionBaseEncapsulation.java
+++ Encapsulation/ConditionBaseEncapsulation.java
@@ -10,8 +10,17 @@ class Student {
             System.out.println("Age cannot be Negative");
         }
     }
+
+    public int getAge() {
+        return age;
+    }
 }
 
 public class ConditionBaseEncapsulation {
-
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.setAge(20);
+       
+        System.out.println("Age : " + s.getAge());
+    }
 }
