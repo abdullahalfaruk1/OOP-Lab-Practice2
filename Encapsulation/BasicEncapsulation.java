087ca673b3ffa9ade9500b25a24ecba1900592f1
Abdullah Al Faruk <alfaruk091@gmail.com>
2026-08-17 10:01:24 +0600

    added a basic encapsulation file

---
+++ Encapsulation/BasicEncapsulation.java
@@ -0,0 +1,27 @@
+package Encapsulation;
+
+class Student {
+    private String name;
+    private int age;
+
+    public Void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age
+    }
+
+}
+
+public class BasicEncapsulation {
+
+}
